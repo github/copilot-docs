fb6806eb457f800e87db6b4052b5bbc74d7f6605
njmsaikat <njmsaikat@gmail.com>
2021-11-03 12:01:00 +0600

    mathematical general operation from comment to code

---
+++ gallery/javascript-comments-to-code/mathematical-operation-javascript.js
@@ -0,0 +1,19 @@
+//addition of two numbers
+function addition(a, b) {
+    return a + b;
+}
+
+//subtraction of two numbers
+function subtraction(a, b) {
+    return a - b;
+}
+
+//multiplication of two numbers
+function multiplication(a, b) {
+    return a * b;
+}
+
+//division of two numbers
+function division(a, b) {
+    return a / b;
+}
