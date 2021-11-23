ecded5e27a15ea426fc2f2165b1c0e04e839dfd9
Saikat Roy <njmsaikat@gmail.com>
2021-11-24 02:27:09 +0600

    Update gallery/javascript-comments-to-code/mathematical-operation-javascript.js
    Co-authored-by: Eric Cheng <ericcheng9316@gmail.com>

--- gallery/javascript-comments-to-code/mathematical-operation-javascript.js
+++ gallery/javascript-comments-to-code/mathematical-operation-javascript.js
@@ -1,24 +1,24 @@
-//addition of two numbers
+// addition of two numbers
 function addition(a, b) {
     return a + b;
 }
 
-//subtraction of two numbers
+// subtraction of two numbers
 function subtraction(a, b) {
     return a - b;
 }
 
-//multiplication of two numbers
+// multiplication of two numbers
 function multiplication(a, b) {
     return a * b;
 }
 
-//division of two numbers
+// division of two numbers
 function division(a, b) {
     return a / b;
 }
 
-//determine if it's a prime number
+// determine if it's a prime number
 function isPrime(num) {
     for (var i = 2; i < num; i++) {
         if (num % i === 0) {
